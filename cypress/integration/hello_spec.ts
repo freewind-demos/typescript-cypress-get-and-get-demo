9061b980b74df794501931b8507375204170c893
Freewind <nowindlee@gmail.com>
2019-06-05 18:19:26 +0800

    demo

--- cypress/integration/hello_spec.ts
+++ cypress/integration/hello_spec.ts
@@ -1,27 +1,18 @@
-describe('TypeScript', () => {
+describe('cypress', () => {
 
   beforeEach(() => {
     cy.visit('index.html');
   });
 
-  it('should use find to find elements based on previous element', () => {
+  it('get and get', () => {
     cy.get('.main')
-      .find('.panel')
-      .should('not.contain', 'Header');
+      .then(ele => {
+        console.log(ele);
+      })
+      .get('.panel')
+      .then(ele => {
+        console.log(ele);
+      })
   });
-
-  it('should alias the container for later use', () => {
-    cy.get('.main').as('main');
-    cy.get('@main').find('.panel1').should('have.text', 'Panel1');
-    cy.get('@main').find('.panel2').should('contain', 'Panel2');
-  });
-
-  it('should use "within" method', () => {
-    cy.get('.main').within(() => {
-      cy.get('.panel1').should('have.text', 'Panel1');
-      cy.get('.panel2').should('contain', 'Panel2');
-    });
-  })
-
 
 })
